7bbc9167da97ab4ec3238b86d93bab6278ccb73b
VivekKumarShah <vivekkushah@gmail.com>
2023-05-09 14:57:36 +0530

    Learnt useEffectHook, API CAlled and implemented conditional render

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { card } from "../constant";
 import { ResList } from "./ResList";
+import { ShimmerUi } from "./ShimmerUi";
 
 export const Body = () => {
     let [valueIn,setValueIn] = useState("")
-    let [list,setList] = useState(card)
-    return (
+    let [list,setList] = useState([]);
+    
+    useEffect(function effect(){
+        getRestaurants()
+    },[])
+
+    async function getRestaurants(){
+        let data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&page_type=DESKTOP_WEB_LISTING");
+        let dataJson = await data.json()
+        console.log(dataJson)
+        setList(dataJson.data.cards[2].data.data.cards)
+        
+    }
+    return list.length===0 ? <ShimmerUi/> : (
         <>
         <input placeholder ="search restaurant" type="text" value={valueIn} onChange={ function onChangeHandler(event){
             setValueIn(event.target.value)
---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,7 @@
+export const ShimmerUi = () => {
+    return (
+      <div>
+        <h1>Shimmer Ui ....................</h1>
+      </div>
+    )
+  }
